46fa1d79fcd23a85fe713d0b7b38f9c96d6868ce
Abdul Siddiq <sfookme@gmail.com>
2016-09-03 16:11:18 +0800

    Added GUI wrapper Fixed/Improved Downloads Added config file (created on first run) to specify osu! install location

--- osuppkg/Program.cs
+++ osuppkg/Program.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Net;
 using System.Reflection;
 using System.Diagnostics;
+using System.Threading;
 
 /*
  *  Right now the code goes my sequential order, thinking of somehow making it modular
@@ -20,61 +21,150 @@ namespace osuppkg
     class Program
     {
 
-        public const string osuBeatmapUrl = "https://osu.ppy.sh/d/";
+        //public const string osuBeatmapUrl = "https://osu.ppy.sh/d/";
+        public const string osuBeatmapUrl = "http://bloodcat.com/osu/s/";
+        public const string osuBeatmapConfigFileName = "osu.conf";
         public static string osuDirectory;
-
-        int progressBar = 0;
-
+        public static string tmpDirectory;
 
         static void Main(string[] args)
         {
 
+            WebRequest.DefaultWebProxy = null;
+            ServicePointManager.DefaultConnectionLimit = 100;
+            
+            if(File.Exists(Directory.GetCurrentDirectory() + "\\" + osuBeatmapConfigFileName))
+            {
+                string[] confs = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\" + osuBeatmapConfigFileName);
+                
+                foreach(string s in confs)
+                {
+                    int i;
+                    if((i = s.IndexOf("InstallLocation=")) >= 0)
+                    {
+                        osuDirectory = s.Substring("InstallLocation=".Length);
+                    }
+                }
+
+                //Defaults to default osu install folder
+                if(osuDirectory == null)
+                {
+                    osuDirectory = Environment.GetEnvironmentVariable("LocalAppData") + "/osu!/Songs";
+                }
+
+            } else {
+
+                Console.WriteLine("Please enter your osu! install location, leave it empty if it is at the default install location.");
+                Console.Write("Location: ");
+                osuDirectory = Console.ReadLine();
+
+                if(osuDirectory == "")
+                {
+                    osuDirectory = Environment.GetEnvironmentVariable("LocalAppData") + "/osu!/Songs";
+                }
+
+                StreamWriter sr = File.CreateText(Directory.GetCurrentDirectory() + "\\" + osuBeatmapConfigFileName);
+                sr.WriteLine("InstallLocation=" + osuDirectory);
+                sr.Close();
+            }
+
+            Console.Clear();
             Console.WriteLine("Welcome to 'osuppkg'\n");
 
-            osuDirectory = Environment.GetEnvironmentVariable("LocalAppData") + "/osu!/Songs";
+
+            tmpDirectory = Directory.GetCurrentDirectory() + "\\tmp\\";
 
             Arguments help = new Arguments("help", "-h", "--help");
             Arguments query = new Arguments("query", "-q", "--query");
             Arguments dl = new Arguments("download", "-d", "--download");
 
-            //foreach (string _args in args) Console.WriteLine("{0}\t", _args);
-
-            if(args.Length > 0)
+            if (args.Length > 0)
             {
                 Arguments a = Arguments.CheckArgsFromIdentifier(args[0].ToLower());
 
-                if (a == Arguments.GetArgsByName("help")) {
+                if (a == Arguments.GetArgsByName("help"))
+                {
                     DisplayHelp();
-                } else if(a == Arguments.GetArgsByName("query"))
+                }
+                else if (a == Arguments.GetArgsByName("query"))
                 {
                     DisplayBeatmaps();
-                } else if (a == Arguments.GetArgsByName("download"))
+                }
+                else if (a == Arguments.GetArgsByName("download")) //Needs rework
                 {
-                    if(args.Length > 1)
+
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\tmp\\");
+
+                    //TODO: Need to rewrite code
+                    //Starting from here
+                    //if (args.Length > 1)
+                    //{
+                    //    //Check if string is a .txt file
+                    //    if (File.Exists(args[1]))
+                    //    {
+                    //        String[] beatmapList = File.ReadAllLines(args[1]);
+                    //        foreach (string s in beatmapList)
+                    //        {
+                    //            String[] tmp = s.Split(' ');
+                    //            string beatmap = osuBeatmapUrl + tmp[0];
+                    //            Process.Start(beatmap);
+                    //        }
+                    //    }
+                    //    else
+                    //    {
+                    //        //Search for osu beatmaps
+                    //    }
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("No beatmap was found..");
+                    //}
+                    //TODO: Need to rewrite code
+                    //Ends here
+                    for (int i = 0; i < args.Length; i++)
                     {
-                        //Check if string is a .txt file
-                        if(File.Exists(args[1]))
+                        if (File.Exists(args[i]))
                         {
                             String[] beatmapList = File.ReadAllLines(args[1]);
-                            foreach (string s in beatmapList) {
+                            foreach (string s in beatmapList)
+                            {
                                 String[] tmp = s.Split(' ');
                                 string beatmap = osuBeatmapUrl + tmp[0];
-                                Process.Start(beatmap);
+                                //Process.Start(beatmap); //DANGEROUS AF
+
+                                Console.Write("Downloading '{0}'\t", beatmap);
+
+                                for(int j = 1; j < tmp.Length; j++)
+                                {
+                                    Console.Write(tmp[j]);
+                                }
+
+                                Console.WriteLine();
+
+                                //using (var progress = new ProgressBar())
+                                //{
+                                //    for (int j = 0; j <= 100; j++)
+                                //    {
+                                //        progress.Report((double)j / 100);
+                                //        Thread.Sleep(20);
+                                //    }
+                                //}
+
+                                using(var client = new WebClient())
+                                {
+                                    client.DownloadFile(beatmap, tmpDirectory + s + ".osz");
+                                }
                             }
-                        } else
-                        {
-                            //Search for osu beatmaps
                         }
-                    } else
-                    {
-                        Console.WriteLine("No beatmap was found..");
                     }
-                } else
+                }
+                else
                 {
                     Console.WriteLine("The command \"{0}\" does not exists.", args[0]);
                     Console.WriteLine("Type -h / -help to display all the commands available");
                 }
-            } else
+            }
+            else
             {
                 DisplayAbout();
             }
@@ -96,11 +186,12 @@ namespace osuppkg
         public static void DisplayBeatmaps()
         {
             string[] s = Directory.GetDirectories(osuDirectory);
-                foreach(string str in s) {
-                    string songdir = "Local/osu!/Songs\\";
-                    int index = str.IndexOf(songdir);
-                    Console.WriteLine("{0}", str.Substring(index + songdir.Length));
-                }
+            foreach (string str in s)
+            {
+                string songdir = "Local/osu!/Songs\\";
+                int index = str.IndexOf(songdir);
+                Console.WriteLine("{0}", str.Substring(index + songdir.Length));
+            }
         }
     }
 }
